b49706b79f194d2cfade4097a05ae0bcb2b88646
KDMEHTA704 <131295594+KDMEHTA704@users.noreply.github.com>
2025-02-11 13:16:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -34,6 +34,18 @@ class Square extends Animal {
     }
 }
 
+const rectangle = new Rectangle(5, 10);
+console.log(rectangle.width); // Output: 5
+console.log(rectangle.height); // Output: 10
+console.log(rectangle.getArea()); // Output: 50
+
+// Creating a Square instance
+const square = new Square(7);
+console.log(square.width); // Output: 7
+console.log(square.height); // Output: 7
+console.log(square.getArea()); // Output: 49
+console.log(square.getPerimeter());
+
 // Do not change the code below this line
 window.Rectangle = Rectangle;
 window.Square = Square;
